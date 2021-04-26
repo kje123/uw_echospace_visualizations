fd4a720cc163831771ff4229a2999d682a29beeb
keith ellingwood <keith-3@hotmail.com>
2021-04-26 14:39:07 -0700

    added data parsing, basemap plot

--- main.py
+++ main.py
@@ -6,5 +6,24 @@ import holoviews as hv
 from holoviews import opts
 hv.extension('matplotlib')
 
-def __main__():
-    
+NC_PATH = 'data/'
+SAVE_PATH = 'plot/'
+
+def main():
+    nc_list = get_nc()
+    basemap = build_basemap(nc_list)
+    hv.save(basemap, 'basemap.png', path=SAVE_PATH)
+
+
+def get_nc():
+    nc_list = glob.glob(NC_PATH + '/*.nc')
+    return nc_list
+
+
+def build_basemap(nc_list):
+    ds_platform = xr.open_mfdataset(nc_list, group='Platform', concat_dim='location_time', combine='by_coords').to_dataframe()
+    pathing_plot = hv.Scatter(ds_platform, vdims='longitude', kdims='latitude')
+    return pathing_plot
+
+if __name__ == '__main__':
+    main()
